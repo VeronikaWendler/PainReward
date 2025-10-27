223dce9f7dabe36c1cbf7245dc2b679ec9973f69
VeronikaWendler <wendler.vroni@gmail.com>
2025-10-27 19:22:02 +0000

    load model 2

--- Hddm_Docker_August_24/DDM_EEG_load.py
+++ Hddm_Docker_August_24/DDM_EEG_load.py
@@ -75,7 +75,7 @@ numba.config.CACHE_ENABLE = False
 # V_sub = value of the worse option
 
 # params:
-version = 1    # defining version #
+version = 2    # defining version #
 run = False        # if True, the the models run, if False the models load
 
 phase = ['dec']  #['ES', 'EE']  # Defines which phase you want ('ES', 'EE', 'LE', or the combinations)
