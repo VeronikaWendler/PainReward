1eee92a9e28a82d13d11855a0539783e821081da
VeronikaWendler <wendler.vroni@gmail.com>
2025-11-08 23:32:12 +0000

    running erp prep

--- EEG/eeg_erp_processing.py
+++ EEG/eeg_erp_processing.py
@@ -20,11 +20,22 @@ from tqdm import tqdm
 
 
 # Set bids directory
-basepath = "D:/Aberdeen_Uni_June24/MPColl_Lab/All_Files_Relevant_For_Git/EEG/PainReward_sub-001-050/painrewardeegdata"
+PROJECT_DIR = Path(os.getenv("PROJECT_DIR", "/workspace"))
+basepath = PROJECT_DIR / "EEG" / "PainReward_sub-001-050" / "painrewardeegdata"
 
-# Choose output directory
+def ensure_dir(path):
+    Path(path).mkdir(parents=True, exist_ok=True)
+import re
+from pathlib import Path
+import os
+
+# disable Numba JIT caching & compilation
+#os.environ["NUMBA_DISABLE_JIT"] = "1"
+import numba
+numba.config.CACHE_ENABLE = False
+
 outpath = opj(basepath, "derivatives")
-layout = BIDSLayout(basepath)
+os.makedirs(outpath, exist_ok=True)
 
 # List participants
 part = [p for p in os.listdir(opj(basepath)) if "sub" in p]
@@ -38,7 +49,7 @@ param = {
     'filtertype': 'fir',
     # Length of baseline
     'erpbaseline': -0.2,
-    'erpepochend': 1.5,
+    'erpepochend': 1,
     # Threshold to reject trials
     'erpreject': dict(eeg=150e-6),
     # Threshold to reject shock trials
@@ -70,7 +81,7 @@ for p in part:
     indir = opj(outpath,  p, 'eeg')
 
     # erp dircetory
-    outdir = opj(outpath,  p, 'eeg', 'erps')              # for averaging over more electrodes : outdir = opj(outpath, p, 'eeg', 'erps_2')
+    outdir = opj(outpath,  p, 'eeg', 'erps')              
     if not os.path.exists(outdir):
         os.mkdir(outdir)
     
@@ -195,10 +206,8 @@ for p in part:
 
 
     # Plot some channels and add to report
-    chans_to_plot = ['Cz', 'Pz', 'CPz', 'Oz', 'Fz', 'FCz', 'POz', 'AF7', 'AF3', 'AFz', 'AF4', 'AF8',
-                     'F7', 'F5', 'F3','F1', 'Fz', 'F2', 'F4', 'F6', 'F8',
-                     'C5', 'C3', 'C1', 'C2', 'C4', 'C6',
-                     'PO7', 'PO3', 'PO4', 'PO8' ]
+    chans_to_plot = ['Fz', 'FCz', 'POz', 'Cz', 'CPz', 'Pz', 'Oz' ]
+    
     figs_chan = []                                                          
     for c in chans_to_plot:                                               
         pick = erp_cues.ch_names.index(c)
@@ -307,7 +316,10 @@ def average_time_win_strials(strials, chans_to_average, amp_lat):
 chans_to_average = [['Fz'], ['FCz'], ['POz'], ['Cz'], ['CPz'], ['Pz'], ['Oz']]    #for averaging over more channels: [['F3'], ['F4'], ['Fz'], ['FC5'], ['FC6'], ['FC1'], ['FC2'], ['FCz'], ['C3'], ['C4'], ['CP1'], ['CP2'], ['CP5'], ['CP6'], ['P3'], ['Pz'], ['P4'], ['P7'], ['P8'], ['PO3'], ['PO7'], ['PO4'], ['O1'], ['Oz'], ['O2']]                                                                          
 amp_lat = [[0.4, 0.8]] 
 
-
+#LPP is centro-parietal
+# N2 is fronto-central
+# P3 is centro-parietal
+# Pain-related N1 is occipito-parietal
 
 all_meta = []
 for p in part:
@@ -327,156 +339,157 @@ all_meta.to_csv(opj(outpath, 'decision_erpsmeta.csv'), index=False)             
 
 
 
-# #_____________________________________________________________________________________________________________________________________________________________
-# # TFR 
-# #_____________________________________________________________________________________________________________________________________________________________
-
-# # importing libraries
-# from mne.report import Report
-# import pprint
-# import mne
-# import os
-# from os.path import join as opj
-# import pandas as pd
-# import numpy as np
-# from mne.viz import plot_evoked_joint as pej
-# from bids import BIDSLayout
-# import matplotlib.pyplot as plt
-# from tqdm import tqdm
-
-# # library for tfr
-# from mne.time_frequency import tfr_morlet
-
-# # bids directory
-# basepath = "D:/Aberdeen_Uni_June24/MPColl_Lab/All_Files_Relevant_For_Git/EEG/PainReward_sub-001-050/painrewardeegdata"
-
-# #output directory
-# outpath = opj(basepath, "derivatives")
-# layout = BIDSLayout(basepath)
-
-# # List participants
-# part = [p for p in os.listdir(opj(basepath)) if "sub" in p]
-# part.sort()
-
-# # set params
-# param = {
-#     # Length of epochs
-#     'erpbaseline': -0.20,  # Used for trial rejection
-#     'erpepochend': 1.5,
-#     'tfrbaseline': -0.50,  # Used for trial rejection
-#     'tfrcropend': 1.5,
-#     'tfrepochstart': -2,  # Used for TFR transform
-#     'tfrepochend': 2,
-#     'ttfreqs': np.arange(4, 101, 1),  # Frequencies
-#     'n_cycles': 0.5*np.arange(4, 101, 1),  # Wavelet cycles
-#     'testresampfreq': 256,  # Sfreq to downsample to
-#     'njobs': 3,  # N cpus to run TFR
-#     # Removed shocked trails
-#     #'ignoreshocks': False,
-# }
-
-# removed_frame = pd.DataFrame(index=part)
-# removed_frame['percleft_cue'] = 999
-# percleft_cue = []
-# percremoved_cue_comperp = []
-
-# for p in tqdm(part):
-#     #--------------------------------------------------------------------------------
-#     # directories
-#     indir = opj(outpath,  p, 'eeg')
-    
-#     # erp dircetory
-#     outdir_erp = opj(outpath,  p, 'eeg', 'erps')
-#     if not os.path.exists(outdir_erp):
-#         os.mkdir(outdir_erp)
-        
-#     # tfr directory
-#     outdir_tfr = opj(outpath, p, 'eeg', 'tfr')
-#     if not os.path.exists(outdir_tfr):
-#         os.mkdir(outdir_tfr)
-        
-#     #--------------------------------------------------------------------------------
-#     # Load cleaned raw file and events
-#     raw = mne.io.read_raw_fif(opj(indir,
-#                                   p + '_decision_cleaned-raw.fif'),
-#                               preload=True)
-#     # get participants events
-#     subject_i = p.split('-')[-1]
-#     # Load trial info in scr data
-#     events = pd.read_csv(layout.get(subject=subject_i, extension='tsv',
-#                                     suffix='events',
-#                                     return_type='filename')[0], sep='\t')
-#     # Get erps metadata
-#     erps = mne.read_epochs(
-#         opj(outdir_erp, p + '_decision_cues_singletrials-epo.fif'))
-#     meta = erps.metadata
-#     allbad = np.sum(meta.badtrial)
-    
-#     #---------------------------------------------------------------------------------
-#     # Epoch according to condition
-#     # Drop unused channels
-#     chans_to_drop = [c for c in ['HEOGL', 'HEOGR', 'VEOGL',
-#                                  'STI 014', 'Status'] if c in raw.ch_names]
-#     raw.drop_channels(chans_to_drop)
-
-#     events['empty'] = 0
-#     events_c = events[events['trial_type'].notna()]
-#     # # Epoch around  off+
-#     events_id = {
-#         "off+":2
-#     }
-    
-#     events_c = events_c[events_c['trial_type'] == 'off+']
-#     events_c['cue_num'] = [events_id[s] for s in events_c.trial_type]
-#     events_cues = np.asarray(events_c[['sample', 'empty', 'cue_num']])
-    
-#     #----------------------------------------------------------------------------------
-#     # Epoch for TFR
-#     tf_cues_strials = mne.Epochs(
-#         raw,
-#         events=events_cues,
-#         event_id=events_id,
-#         tmin=param['tfrepochstart'],
-#         baseline=None,
-#         metadata=meta,
-#         tmax=param['tfrepochend'],
-#         preload=True,
-#         verbose=False)
-
-#     # # TFR single trials
-#     strials = tfr_morlet(
-#         tf_cues_strials,
-#         freqs=param['ttfreqs'],
-#         n_cycles=param['n_cycles'],
-#         return_itc=False,
-#         use_fft=True,
-#         decim=int(1024/param["testresampfreq"]),
-#         n_jobs=param['njobs'],
-#         average=False)
-
-#     # Clear for memory
-#     tf_cues_strials = None
-    
-#     # Remove unused part
-#     strials.crop(tmin=param['tfrbaseline'],
-#                  tmax=param['tfrcropend'])
-
-#     # Check drop statistics
-#     percleft_cue.append(
-#         (len(strials) - np.sum(meta.badtrial))/len(strials)*100)
-#     percremoved_cue_comperp.append(100-((125 - allbad)/125*100))
-    
-    
-#     #----------------------------------------------------------------------------------
-#     # save tfr 
-#     strials.save(opj(outdir_tfr,  p + '_decision_cues_'
-#                      + 'epochs-tfr.h5'), overwrite=True)
-#     # clear for memory
-#     strials = None  # Clear for memory
-    
-# removed_frame['percleft_cue'] = percleft_cue
-# removed_frame['percremoved_cue_comperp'] = percremoved_cue_comperp
-# removed_frame.to_csv(opj(outpath, 'decision_tfr_rejectionstats.csv'))
-
-
-# %%
+## #_____________________________________________________________________________________________________________________________________________________________
+## # TFR 
+## #_____________________________________________________________________________________________________________________________________________________________
+#
+## importing libraries
+#from mne.report import Report
+#import pprint
+#import mne
+#import os
+#from os.path import join as opj
+#import pandas as pd
+#import numpy as np
+#from mne.viz import plot_evoked_joint as pej
+#from bids import BIDSLayout
+#import matplotlib.pyplot as plt
+#from tqdm import tqdm
+#
+## library for tfr
+#from mne.time_frequency import tfr_morlet
+#
+## bids directory
+#basepath = "D:/Aberdeen_Uni_June24/MPColl_Lab/All_Files_Relevant_For_Git/EEG/PainReward_sub-001-050/painrewardeegdata"
+#
+##output directory
+#outpath = opj(basepath, "derivatives")
+#layout = BIDSLayout(basepath)
+#
+## List participants
+#part = [p for p in os.listdir(opj(basepath)) if "sub" in p]
+#part.sort()
+#
+## set params
+#param = {
+#    # Length of epochs
+#    'erpbaseline': -0.20,  # Used for trial rejection
+#    'erpepochend': 1.5,
+#    'tfrbaseline': -0.50,  # Used for trial rejection
+#    'tfrcropend': 1.5,
+#    'tfrepochstart': -2,  # Used for TFR transform
+#    'tfrepochend': 2,
+#    'ttfreqs': np.arange(4, 101, 1),  # Frequencies
+#    'n_cycles': 0.5*np.arange(4, 101, 1),  # Wavelet cycles
+#    'testresampfreq': 256,  # Sfreq to downsample to
+#    'njobs': 3,  # N cpus to run TFR
+#    # Removed shocked trails
+#    #'ignoreshocks': False,
+#}
+#
+#removed_frame = pd.DataFrame(index=part)
+#removed_frame['percleft_cue'] = 999
+#percleft_cue = []
+#percremoved_cue_comperp = []
+#
+#for p in tqdm(part):
+#    #--------------------------------------------------------------------------------
+#    # directories
+#    indir = opj(outpath,  p, 'eeg')
+#    
+#    # erp dircetory
+#    outdir_erp = opj(outpath,  p, 'eeg', 'erps')
+#    if not os.path.exists(outdir_erp):
+#        os.mkdir(outdir_erp)
+#        
+#    # tfr directory
+#    outdir_tfr = opj(outpath, p, 'eeg', 'tfr')
+#    if not os.path.exists(outdir_tfr):
+#        os.mkdir(outdir_tfr)
+#        
+#    #--------------------------------------------------------------------------------
+#    # Load cleaned raw file and events
+#    raw = mne.io.read_raw_fif(opj(indir,
+#                                  p + '_decision_cleaned-raw.fif'),
+#                              preload=True)
+#    # get participants events
+#    subject_i = p.split('-')[-1]
+#    # Load trial info in scr data
+#    events = pd.read_csv(layout.get(subject=subject_i, extension='tsv',
+#                                    suffix='events',
+#                                    return_type='filename')[0], sep='\t')
+#    # Get erps metadata
+#    erps = mne.read_epochs(
+#        opj(outdir_erp, p + '_decision_cues_singletrials-epo.fif'))
+#    meta = erps.metadata
+#    allbad = np.sum(meta.badtrial)
+#    
+#    #---------------------------------------------------------------------------------
+#    # Epoch according to condition
+#    # Drop unused channels
+#    chans_to_drop = [c for c in ['HEOGL', 'HEOGR', 'VEOGL',
+#                                 'STI 014', 'Status'] if c in raw.ch_names]
+#    raw.drop_channels(chans_to_drop)
+#
+#    events['empty'] = 0
+#    events_c = events[events['trial_type'].notna()]
+#    # # Epoch around  off+
+#    events_id = {
+#        "off+":2
+#    }
+#    
+#    events_c = events_c[events_c['trial_type'] == 'off+']
+#    events_c['cue_num'] = [events_id[s] for s in events_c.trial_type]
+#    events_cues = np.asarray(events_c[['sample', 'empty', 'cue_num']])
+#    
+#    #----------------------------------------------------------------------------------
+#    # Epoch for TFR
+#    tf_cues_strials = mne.Epochs(
+#        raw,
+#        events=events_cues,
+#        event_id=events_id,
+#        tmin=param['tfrepochstart'],
+#        baseline=None,
+#        metadata=meta,
+#        tmax=param['tfrepochend'],
+#        preload=True,
+#        verbose=False)
+#
+#    # # TFR single trials
+#    strials = tfr_morlet(
+#        tf_cues_strials,
+#        freqs=param['ttfreqs'],
+#        n_cycles=param['n_cycles'],
+#        return_itc=False,
+#        use_fft=True,
+#        decim=int(1024/param["testresampfreq"]),
+#        n_jobs=param['njobs'],
+#        average=False)
+#
+#    # Clear for memory
+#    tf_cues_strials = None
+#    
+#    # Remove unused part
+#    strials.crop(tmin=param['tfrbaseline'],
+#                 tmax=param['tfrcropend'])
+#
+#    # Check drop statistics
+#    percleft_cue.append(
+#        (len(strials) - np.sum(meta.badtrial))/len(strials)*100)
+#    percremoved_cue_comperp.append(100-((125 - allbad)/125*100))
+#    
+#    
+#    #----------------------------------------------------------------------------------
+#    # save tfr 
+#    strials.save(opj(outdir_tfr,  p + '_decision_cues_'
+#                     + 'epochs-tfr.h5'), overwrite=True)
+#    # clear for memory
+#    strials = None  # Clear for memory
+#    
+#removed_frame['percleft_cue'] = percleft_cue
+#removed_frame['percremoved_cue_comperp'] = percremoved_cue_comperp
+#removed_frame.to_csv(opj(outpath, 'decision_tfr_rejectionstats.csv'))
+#
+#
+## %%
+#
