3c3dde19ebfeead2f4bcda297c8cfdbe5e981070
VeronikaWendler <wendler.vroni@gmail.com>
2025-11-09 15:44:51 +0000

    run eeg erp prep 2

--- run_EEG.sh
+++ run_EEG.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #SBATCH --job-name=eeg_clean
 #SBATCH --partition=compute                
-#SBATCH --cpus-per-task=12                  
+#SBATCH --cpus-per-task=8                  
 #SBATCH --mem=180G                          # EEG 
 #SBATCH --time=24:00:00                    
 #SBATCH -o logs/eeg_%j.out
