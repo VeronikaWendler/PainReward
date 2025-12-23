24e05f0c4dc9eb882805121aad8724ed87a71567
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-23 22:58:46 +0100

    joint

--- EEG/eeg_erp_groupplots_cues.py
+++ EEG/eeg_erp_groupplots_cues.py
@@ -38,7 +38,7 @@ layout = BIDSLayout(outpathall)
 
 version = 11# 1 for decision phase, 2 for passive phase, 3 = decision RT + 3 GLMs
 
-v11_mode = "separate"   # "separate" or "joint"
+v11_mode = "joint"   # "separate" or "joint"
 
 
 # noz     - NO_Zscoring      (raw regressors + raw RT)
