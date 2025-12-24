f5621d33f18cb394676cd5ca0636bcd62c43c2d0
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-24 13:48:08 +0100

    ....

--- EEG/eeg_erp_groupplots_cues.py
+++ EEG/eeg_erp_groupplots_cues.py
@@ -40,7 +40,7 @@ version = 14   # 1 for decision phase, 2 for passive phase, 3 = decision RT + 3 
 
 v13_mode = "joint"   # "separate" or "joint"
 v11_mode = "joint"
-v14_mode = "separate"
+v14_mode = "joint"
 
 # noz     - NO_Zscoring      (raw regressors + raw RT)
 # z       - Zscoring         (z-scored regressors + z-scored RT)
