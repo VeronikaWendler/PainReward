b0a2d3b1dd1f4d7fa85e5c2ea917825114510821
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-25 13:37:00 +0100

    ...

--- EEG/eeg_erp_massunivariate_cues.py
+++ EEG/eeg_erp_massunivariate_cues.py
@@ -62,7 +62,7 @@ version = 14    # version 1 is for decision and version 2 is for passive phase
 v11_mode = "joint"   # or "joint"s
 
 v13_mode = "joint"   # or "joint"s
-v14_mode = "joint"
+v14_mode = "separate"
 v15_mode = "joint"
 
 if version == 1:
