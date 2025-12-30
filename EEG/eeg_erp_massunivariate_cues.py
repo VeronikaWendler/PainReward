85dc4d6d79c2c73906e6cd9e1d2ed676a25e34bf
VeronikaWendler <wendler.vroni@gmail.com>
2025-12-30 23:14:42 +0100

    ...

--- EEG/eeg_erp_massunivariate_cues.py
+++ EEG/eeg_erp_massunivariate_cues.py
@@ -59,7 +59,7 @@ if not os.path.exists(outpath):
     
 # here for decision its just erps_massuni_drift_mod_9 and for passive it is: erps_massuni_drift_mod_9_2_passive
 version = 22
-v22_mode = "joint"   # "joint" or "separate"
+v22_mode = "separate"   # "joint" or "separate"
 
 
 v13_mode = "joint"   # or "joint"s
