8f3245e5fddb6b7dc3f9d1c98139a9cc9abf6769
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-02 22:59:01 +0100

    ...

--- EEG/eeg_erp_groupplots_cues.py
+++ EEG/eeg_erp_groupplots_cues.py
@@ -296,8 +296,8 @@ elif version == 27:
 elif version == 28:
     base_v28 = opj(outpathall, "statistics_new/erps_massuni_sv_cuelong")
     if v28_mode == "joint":
-        outpath = opj(base_v28, "v26_low_STA_SAI_SV_joint")
-        outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v33_jointGLM_low_STA_SAI_SV_RT")
+        outpath = opj(base_v28, "v26_high_STA_SAI_SV_joint")
+        outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v33_jointGLM_high_STA_SAI_SV_RT")
     elif v28_mode == "separate":
         outpath = opj(base_v28, "v26_low_STA_SAI_SV_sep")
         outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v33_sepGLM_low_STA_SAI_SV_RT")
