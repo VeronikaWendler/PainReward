15e5f28a411442ca88b27a96966cdc3541b42f2a
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-02 19:21:14 +0100

    ...

--- EEG/eeg_erp_massunivariate_cues.py
+++ EEG/eeg_erp_massunivariate_cues.py
@@ -58,8 +58,8 @@ if not os.path.exists(outpath):
     os.mkdir(outpath)
     
 # here for decision its just erps_massuni_drift_mod_9 and for passive it is: erps_massuni_drift_mod_9_2_passive
-version = 27
-v27_mode = "joint"   # "joint" or "separate"
+version = 26
+v26_mode = "joint"   # "joint" or "separate"
 
 
 v13_mode = "joint"   # or "joint"s
@@ -293,9 +293,9 @@ elif version == 26:
     base_v26 = opj(outpath, "erps_massuni_sv_cuelong")
     os.makedirs(base_v26, exist_ok=True)
     if v26_mode == "joint":
-        outpath = opj(base_v26, "v26_low_STA_TAI_SV_joint")
+        outpath = opj(base_v26, "v26_high_STA_TAI_SV_joint")
     elif v26_mode == "separate":
-        outpath = opj(base_v26, "v26_low_STA_TAI_SV_sep")
+        outpath = opj(base_v26, "v26_high_STA_TAI_SV_sep")
     else:
         raise ValueError("v25_mode must be 'joint' or 'separate'")
     os.makedirs(outpath, exist_ok=True)
@@ -462,7 +462,7 @@ if version == 25:
     
 if version == 26:
     mod_data["sta_tai_mediansplit"] = pd.to_numeric(mod_data["sta_tai_mediansplit"], errors="coerce")
-    mod_data = mod_data.loc[mod_data["sta_tai_mediansplit"] == 0].copy()
+    mod_data = mod_data.loc[mod_data["sta_tai_mediansplit"] == 1].copy()
     print("After high sta_tai_mediansplit (median split) filter, mod_data rows:", len(mod_data))
     print("After high sta_tai_mediansplit (median split) filter, unique participants:", mod_data["participant"].nunique())
     
