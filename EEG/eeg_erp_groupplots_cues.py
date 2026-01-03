c7e2d2969b7aef08d1c2f7bc234e738f837db200
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-03 19:35:13 +0100

    ...

--- EEG/eeg_erp_groupplots_cues.py
+++ EEG/eeg_erp_groupplots_cues.py
@@ -321,8 +321,8 @@ elif version == 29:
 elif version == 30:
     base_v30 = opj(outpathall, "statistics_new/erps_massuni_sv_cuelong")
     if v30_mode == "joint":
-        outpath = opj(base_v30, "v30_female_joint")
-        outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v35_jointGLM_female_RT")
+        outpath = opj(base_v30, "v30_male_joint")
+        outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v35_jointGLM_male_RT")
     elif v30_mode == "separate":
         outpath = opj(base_v30, "v30_female_sep")
         outfigpath = opj(outpathall, "figures/erps_massuni_drift_sv_subsetOV/v35_sepGLM_female_RT")
