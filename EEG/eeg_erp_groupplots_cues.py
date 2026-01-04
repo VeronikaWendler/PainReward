58c28fcc586974f773c2a721a1a740790d9008e7
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-04 22:43:20 +0100

    ...

--- EEG/eeg_erp_groupplots_cues.py
+++ EEG/eeg_erp_groupplots_cues.py
@@ -37,12 +37,12 @@ layout = BIDSLayout(inpath)
 part = pd.read_csv(opj(inpath, 'participants.tsv'), sep='\t')
 layout = BIDSLayout(outpathall)
 
-version = 37  # 1 for decision phase, 2 for passive phase, 3 = decision RT + 3 GLMs
+version = 38  # 1 for decision phase, 2 for passive phase, 3 = decision RT + 3 GLMs
 
 v13_mode = "joint"   # "separate" or "joint"
 v11_mode = "joint"
 v14_mode = "joint"
-v37_mode = "separate"
+v38_mode = "separate"
 
 # noz     - NO_Zscoring      (raw regressors + raw RT)
 # z       - Zscoring         (z-scored regressors + z-scored RT)
