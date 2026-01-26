e1e77f92f723e0710c9ff6a35668f6d703b38c03
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-26 16:58:26 +0000

    loading mne-iclabel

--- run_EEG.sh
+++ run_EEG.sh
@@ -38,6 +38,8 @@ export OUT_DIR="/data/derivatives"
 apptainer exec \
   --bind "${PROJECT}:${PROJECT_DIR}" \
   --bind "${DATA_ROOT}:${DATA_DIR}" \
+  --bind "$HOME/pydeps:/pydeps" \
+  --env PYTHONPATH="/pydeps:${PYTHONPATH}" \
   "${IMAGE}" \
   python "${PROJECT_DIR}/EEG/eeg_preprocess.py"
 
