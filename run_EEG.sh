abe4ee360632ada6fe2a440a75c74573122bb745
VeronikaWendler <wendler.vroni@gmail.com>
2026-01-26 16:16:54 +0000

    some bash mistake

--- run_EEG.sh
+++ run_EEG.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-#SBATCH --job-name=eeg_clean
-#SBATCH --partition=compute
-#SBATCH --cpus-per-task=8
-#SBATCH --mem=180G
+#SBATCH --job-name=eeg_prep
 #SBATCH --time=24:00:00
+#SBATCH --cpus-per-task=8
+#SBATCH --mem=128G
 #SBATCH --output=logs/eeg_%j.out
 #SBATCH --error=logs/eeg_%j.err
 #SBATCH --mail-type=ALL
