0d5f3e24345efb3b6bccbbe6025bb3d72dfef62d
VeronikaWendler <wendler.vroni@gmail.com>
2026-02-08 19:42:08 +0000

    plot erp rp -800 to -0.05 v3

--- run_EEG.sh
+++ run_EEG.sh
@@ -46,4 +46,4 @@ apptainer exec --cleanenv \
   --env OUT_DIR="${OUT_DIR}" \
   --env HDDM_DIR="${HDDM_DIR}" \
   "${IMAGE}" \
-  python "${PROJECT_DIR}/EEG/eeg_erp_rp.py"
+  python "${PROJECT_DIR}/EEG/eeg_erp_groupplots_rp.py"
