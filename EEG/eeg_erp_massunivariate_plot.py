ee34e48b457a1e5e07537dd6352621a18ea3c5a6
VeronikaWendler <wendler.vroni@gmail.com>
2026-03-05 12:20:10 +0000

    eeg_erp_massunivariate_plot passive

--- EEG/eeg_erp_massunivariate_plot.py
+++ EEG/eeg_erp_massunivariate_plot.py
@@ -40,7 +40,7 @@ layout = BIDSLayout(basepath)
 # Version + GLM version (keep version-sensitive structure)
 # version = 2 -> decision plotting (UNCHANGED)
 # version = 1 -> passive plotting (ADDED; mirrors decision plotting)
-version = 2
+version = 1
 glm_version = "z"  # noz / z / partz
 
 # ---------------------------------------------------------------------------------------------------
