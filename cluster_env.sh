8f0351c7d9213fe87fd3005448ad4f5a7029886b
Veronika Wendler (PhD Psychology Lab OS UKRI FT) <vaw508@bear-pg-login06.bear.cluster>
2026-07-03 14:23:27 +0100

    Add cluster environment and sanity-check SLURM jobs

---
+++ cluster_env.sh
@@ -0,0 +1,14 @@
+export CODE_ROOT="$HOME/projects/PainReward_mpreview_cluster"
+export ORIGINAL_BIDS="/rds/projects/z/zhanglp-vwendler-core/PainReward_ULaval/EEG/PainReward_sub-001-050/painrewardeegdata"
+export RUN_ROOT="/rds/projects/z/zhanglp-vwendler-core/PainReward_mpreview_run"
+
+export basepath="$RUN_ROOT"
+export PROJECT_DIR="$RUN_ROOT"
+export HDDM_DIR="$RUN_ROOT/derivatives/hddm"
+
+export IMAGE_MNE="$HOME/containers/mne_latest.sif"
+export IMAGE_HDDM="$HOME/containers/hddm_latest.sif"
+
+export MPLBACKEND=Agg
+export PYTHONUNBUFFERED=1
+export PYTHONNOUSERSITE=1
